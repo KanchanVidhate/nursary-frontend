3f638b575df6de32ac1f72372c855c71b41a8ad7
Kanchan Vidhate <kvidhate34@gmail.com>
2024-07-24 12:36:09 +0530

    client add

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider,createBrowserRouter } from 'react-router-dom';
+import Home from "./views/Home/Home.js";
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Home/>
+  },
+  {
+    path:"*",
+    element:<h1>404 Not Found</h1>
+  }
+]);
+
+root.render((<div>
+  <RouterProvider router={router}/>
+  
+</div>));
+
